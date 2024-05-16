d53df0fb1f00e5b355020bc68639a82f4c3d7026
CoolHandAR <radzausrys1@gmail.com>
2024-05-16 12:52:21 +0300

    s

--- liteCraft/src/sound/sound.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <cglm/cglm.h>
-#include "miniaudio/miniaudio.h"
-
-ma_sound* Sound_get(const char* p_soundName);
-ma_sound* Sound_load(const char* p_soundName, const char* p_filePath, uint32_t p_flags);
-void Sound_play(const char* p_soundName);
-void Sound_stop(const char* p_soundName);
